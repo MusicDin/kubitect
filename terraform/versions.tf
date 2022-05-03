3f7db3f42bd4fc681410e26ba9c11397b83d6f30
Din Music <din.music@din-cloud.com>
2022-05-03 15:15:32 +0200

    Separate terraform state file from terraform source

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,12 @@
 terraform {
+
   experiments      = [module_variable_optional_attrs]
   required_version = ">= 1.1.4"
+
+  backend "local" {
+    path = "../config/terraform/terraform.tfstate"
+  }
+
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
