292d9a38c1ec242e9870655e1e05df4e551d98e4
Din Music <din.music@din-cloud.com>
2022-05-10 12:29:20 +0200

    Bump CLI version to the 2.0.5

--- cli/env/constatnts.go
+++ cli/env/constatnts.go
@@ -6,7 +6,7 @@ const (
 	ConstProjectHomeDir     = ".kubitect"
 	ConstProjectClustersDir = "clusters"
 	ConstProjectUrl         = "https://github.com/MusicDin/kubitect"
-	ConstProjectVersion     = "v2.0.4"
+	ConstProjectVersion     = "v2.0.5"
 	ConstTerraformVersion   = "1.1.4"
 	ConstTerraformStatePath = "config/terraform/terraform.tfstate"
 	ConstKubeconfigPath     = "config/admin.conf"
