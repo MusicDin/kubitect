06559b62991a9aaba799ae37d0e1485b3ec9f389
Seumas Dunlop <seumas.dunlop@coralbay.tv>
2022-08-18 13:24:33 +1200

    Fix terraform error when VM's IP can't be read

--- terraform/modules/vm/output.tf
+++ terraform/modules/vm/output.tf
@@ -3,7 +3,7 @@ output "vm_info" {
     id   = var.vm_id
     type = var.vm_type
     name = libvirt_domain.vm_domain.name,
-    ip   = libvirt_domain.vm_domain.network_interface.0.addresses.0
+    ip   = try(libvirt_domain.vm_domain.network_interface.0.addresses.0, null)
     dataDisks = [
       for disk in var.vm_data_disks : {
         name = disk.name
