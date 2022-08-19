7df995dd0de58830935041061e0fdb68d30f5032
Din Music <din.music@din-cloud.com>
2022-08-19 13:20:15 +0200

    Fix tf failure when dataResroucePool is null

--- terraform/modules/host/main.tf
+++ terraform/modules/host/main.tf
@@ -152,7 +152,7 @@ module "master_module" {
   vm_cpu               = each.value.cpu != null ? each.value.cpu : var.cluster_nodes_master_default_cpu
   vm_ram               = each.value.ram != null ? each.value.ram : var.cluster_nodes_master_default_ram
   vm_main_disk_size    = each.value.mainDiskSize != null ? each.value.mainDiskSize : var.cluster_nodes_master_default_mainDiskSize
-  vm_data_disks        = [for disk in each.value.dataDisks : disk if disk.pool == null || disk.pool == "main" || length(setintersection(keys(libvirt_pool.data_resource_pools), [disk.pool])) == 1]
+  vm_data_disks        = [for disk in (each.value.dataDisks != null ? each.value.dataDisks : []) : disk if disk.pool == null || disk.pool == "main" || length(setintersection(keys(libvirt_pool.data_resource_pools), [disk.pool])) == 1]
   vm_id                = each.value.id
   vm_mac               = each.value.mac
   vm_ip                = each.value.ip
@@ -199,7 +199,7 @@ module "worker_module" {
   vm_cpu               = each.value.cpu != null ? each.value.cpu : var.cluster_nodes_worker_default_cpu
   vm_ram               = each.value.ram != null ? each.value.ram : var.cluster_nodes_worker_default_ram
   vm_main_disk_size    = each.value.mainDiskSize != null ? each.value.mainDiskSize : var.cluster_nodes_worker_default_mainDiskSize
-  vm_data_disks        = [for disk in each.value.dataDisks : disk if disk.pool == null || disk.pool == "main" || length(setintersection(keys(libvirt_pool.data_resource_pools), [disk.pool])) == 1]
+  vm_data_disks        = [for disk in (each.value.dataDisks != null ? each.value.dataDisks : []) : disk if disk.pool == null || disk.pool == "main" || length(setintersection(keys(libvirt_pool.data_resource_pools), [disk.pool])) == 1]
   vm_id                = each.value.id
   vm_mac               = each.value.mac
   vm_ip                = each.value.ip
