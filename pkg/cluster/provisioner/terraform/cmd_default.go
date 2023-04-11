5c5805faf6f0ac3445e85c4df3b5a9147c946e7e
Din Mušić <din.music@din-cloud.com>
2023-04-11 13:57:15 +0200

    Release 3.0.1 (#121)
    * Fix unknown Pdeathsig field on darwin (#118)
    
    * Update docs with zsh autocomplete (#120)

---
+++ pkg/cluster/provisioner/terraform/cmd_default.go
@@ -0,0 +1,42 @@
+//go:build !linux
+// +build !linux
+
+package terraform
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/MusicDin/kubitect/pkg/ui"
+)
+
+// runCmd runs terraform command and returns exit code with
+// a potential error.
+func (t *terraform) runCmd(action string, args []string, showOutput bool) (int, error) {
+	args = append([]string{action}, args...)
+
+	if !ui.HasColor() {
+		args = append(args, flag("no-color"))
+	}
+
+	cmd := exec.Command(t.binPath, args...)
+	cmd.Dir = t.projectDir
+
+	cmd.Stderr = ui.Streams().Err().File()
+	if showOutput || ui.Debug() {
+		cmd.Stdout = ui.Streams().Out().File()
+	}
+
+	if ui.Debug() {
+		cmd.Env = append(cmd.Env, "TF_LOG=INFO")
+	}
+
+	err := cmd.Run()
+	exitCode := cmd.ProcessState.ExitCode()
+
+	if err != nil {
+		err = fmt.Errorf("terraform %s failed: %v", action, err)
+	}
+
+	return exitCode, err
+}
