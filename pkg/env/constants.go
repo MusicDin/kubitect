9265e2ca9dde6793590db61d47aa2726ca8ea67b
Din Mušić <din.music@din-cloud.com>
2023-06-02 21:02:44 +0200

    Fix local cluster search causes duplicate names (#140)

--- pkg/env/constants.go
+++ pkg/env/constants.go
@@ -7,7 +7,7 @@ package env
 // Project related constants
 const (
 	ConstProjectUrl        = "https://github.com/MusicDin/kubitect"
-	ConstProjectVersion    = "v3.1.0"
+	ConstProjectVersion    = "v3.1.1"
 	ConstKubesprayUrl      = "https://github.com/kubernetes-sigs/kubespray"
 	ConstKubesprayVersion  = "v2.21.0"
 	ConstKubernetesVersion = "v1.25.6"
