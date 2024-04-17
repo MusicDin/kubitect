9374c445cc5db4fd1305d43eaa1f6403accf2804
Din Music <din.music@din-cloud.com>
2024-04-17 20:07:59 +0200

    scripts: Simplify waiting for nodes and pods readiness
    Signed-off-by: Din Music <din.music@din-cloud.com>

--- scripts/test-cluster.sh
+++ scripts/test-cluster.sh
@@ -19,62 +19,14 @@ defer() {
 FAIL=1
 trap defer EXIT HUP INT TERM
 
-echo "==> TEST: Cluster readiness"
+echo "==> TEST: All nodes ready"
+kubectl wait --for=condition=ready node --all --timeout=120s
 
 startTime=$(date +%s)
 nodes=$(kubectl get nodes | awk 'NR>1 {print $1}')
 
-for node in $nodes; do
-    while :; do
-        isReady=$(kubectl get node "${node}" \
-            -o jsonpath='{.status.conditions[?(@.type=="Ready")].status}'
-        )
-
-        if [ "${isReady}" = "True" ]; then
-            echo "===> PASS: Node ${node} is ready."
-            break
-        fi
-
-        currentTime=$(date +%s)
-        elapsedTime=$((currentTime - timeStart))
-
-        if [ "${elapsedTime}" -gt "${TIMEOUT}" ]; then
-            echo "FAIL: Node ${node} is NOT READY after ${TIMEOUT} seconds!"
-            kubectl get nodes
-            break
-        fi
-
-        sleep 10
-    done
-done
-
-echo "==> TEST: Running pods"
-
-startTime=$(date +%s)
-
-while :; do
-    failedPods=$(kubectl get pods \
-        --all-namespaces \
-        --field-selector="status.phase!=Succeeded,status.phase!=Running" \
-        --output custom-columns="NAMESPACE:metadata.namespace,POD:metadata.name,STATUS:status.phase"
-    )
-
-    if [ "$(echo "${failedPods}" | awk 'NR>1')" = "" ]; then
-        echo "===> PASS: All pods are running."
-        break
-    fi
-
-    currentTime=$(date +%s)
-    elapsedTime=$((currentTime - startTime))
-
-    if [ "${elapsedTime}" -gt "${TIMEOUT}" ]; then
-        echo "==> FAIL: Pods not running after ${TIMEOUT} seconds!"
-        echo "${failedPods}"
-        break
-    fi
-
-    sleep 10
-done
+echo "==> TEST: All pods ready"
+kubectl wait --for=condition=ready pods --all -A --timeout=120s
 
 echo "==> TEST: DNS"
 kubectl apply -f https://k8s.io/examples/admin/dns/dnsutils.yaml
